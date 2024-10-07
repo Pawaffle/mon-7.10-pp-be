dfc700e023eb06dafb7492baf719f7652e716d50
pawaffle <pdegterev@gmail.com>
2024-10-07 14:22:08 +0300

    user swagger

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const todoTaskRouter = require("./routers/todoTaskRouter");
 const userRouter = require("./routers/userRouter");
 const tourRouter = require("./routers/tourRouter");
 
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger_user.yaml"); // Load the YAML file Load swagger.json
+// const swaggerDocument = YAML.load("./swagger_tour.yaml");
+
 // express app
 const app = express();
 
@@ -21,7 +26,10 @@ app.use(requestLogger);
 app.get("/", (req, res) => res.send("API Running!"));
 
 app.use("/api/todoTasks", todoTaskRouter);
+
 app.use("/api/users", userRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api/tours", tourRouter);
 
 app.use(unknownEndpoint);
